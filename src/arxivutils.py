6884e26e37e2052320c1bfa8be2481ad182168a6
Waqas Bhatti on namazu <waqas@pha.jhu.edu>
2014-06-15 20:38:18 -0400

    added arxivutils to download arxiv listings using Selenium and parse them using BeautifulSoup

---
+++ src/arxivutils.py
@@ -0,0 +1,191 @@
+'''
+utils.py - Waqas Bhatti (wbhatti@astro.princeton.edu) - Jun 2014
+
+Contains useful things for the astroph-coffee server.
+
+'''
+
+import os
+import os.path
+import sqlite3
+import ConfigParser
+import random
+import time
+from datetime import date, datetime
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+from tornado.escape import squeeze
+
+
+def get_page_html(url, fakery=True):
+    '''
+    This connects to the arxiv server and downloads the HTML of the page, while
+    faking some activity.
+
+    '''
+
+    driver = webdriver.Firefox()
+    driver.get(url)
+    html = driver.page_source
+
+    time.sleep(5.0)
+
+    if fakery:
+
+        npagedowns = 2 + int(random.random()*20.0)
+        for x in range(npagedowns):
+            elem = driver.find_element_by_tag_name('body')
+            elem.send_keys(Keys.PAGE_DOWN)
+            time.sleep(10.0 + random.random()*10.0)
+
+    # then quit
+    driver.quit()
+
+    return html
+
+
+def soupify(htmldoc):
+    '''
+    Loads the HTML document obtained by get_page_html into an instance of
+    BeautifulSoup.
+
+    '''
+
+    return BeautifulSoup(htmldoc)
+
+
+def get_arxiv_lists(soup):
+    '''
+    This gets a list of articles from the soupified HTML document.
+
+    '''
+
+    docparts = soup.find_all('dl')
+
+    papers, crosslists, replacements = docparts
+
+
+    paperlinks, paperdata = (papers.find_all('dt'),
+                             papers.find_all('div', class_='meta'))
+    crosslinks, crossdata = (crosslists.find_all('dt'),
+                             crosslists.find_all('div', class_='meta'))
+
+
+    return paperlinks, paperdata, crosslinks, crossdata
+
+
+
+def get_arxiv_articles(paperlinks, paperdata, crosslinks, crossdata):
+
+    paperdict = {}
+    crossdict = {}
+
+    for ind, link, data in zip(range(len(paperlinks)), paperlinks, paperdata):
+
+        paper_abstract = squeeze(data.p.text.replace('\n',' ').strip())
+        paper_title = squeeze(
+            data.find_all(
+                'div',class_='list-title'
+            )[0].text.strip('\n').lstrip('Title: ')
+        )
+
+        paper_authors = (
+            data.find_all(
+                'div',class_='list-authors'
+            )[0].text.strip('\n').lstrip('Authors: ')
+            )
+        paper_authors = [squeeze(x.lstrip('\n').rstrip('\n'))
+                         for x in paper_authors.split(', ')]
+
+        paper_links = link.find_all('a')[1:3]
+        paper_link, arxiv_id = paper_links[0]['href'], paper_links[0].text
+        paper_pdf = paper_links[1]['href']
+
+        try:
+            comment_contents = data.find('div',class_='list-comments').contents[2:]
+            paper_comments = squeeze(' '.join(
+                [str(x).lstrip('\n').rstrip('\n') for x in comment_contents]
+                ).strip())
+
+        except AttributeError:
+            paper_comments = ''
+
+
+        paperdict[ind+1] = {'authors':paper_authors,
+                            'title':paper_title,
+                            'abstract':paper_abstract,
+                            'comments':paper_comments,
+                            'arxiv':arxiv_id,
+                            'link':paper_link,
+                            'pdf':paper_pdf}
+
+
+    for ind, link, data in zip(range(len(crosslinks)), crosslinks, crossdata):
+
+        cross_abstract = squeeze(data.p.text.replace('\n',' ').strip())
+        cross_title = squeeze(
+            data.find_all(
+                'div',class_='list-title'
+            )[0].text.strip('\n').lstrip('Title: ')
+        )
+
+        cross_authors = (
+            data.find_all(
+                'div',class_='list-authors'
+            )[0].text.strip('\n').lstrip('Authors: ')
+            )
+        cross_authors = [squeeze(x.lstrip('\n').rstrip('\n'))
+                         for x in cross_authors.split(', ')]
+
+        cross_links = link.find_all('a')[1:3]
+        cross_link, arxiv_id = cross_links[0]['href'], cross_links[0].text
+        cross_pdf = cross_links[1]['href']
+
+        try:
+            comment_contents = data.find('div',class_='list-comments').contents[2:]
+            cross_comments = squeeze(
+                ' '.join(
+                    [str(x).lstrip('\n').rstrip('\n') for x in comment_contents]
+                ).strip()
+            )
+
+        except AttributeError:
+            cross_comments = ''
+
+        crossdict[ind+1] = {'authors':cross_authors,
+                            'title':cross_title,
+                            'abstract':cross_abstract,
+                            'comments':cross_comments,
+                            'arxiv':arxiv_id,
+                            'link':cross_link,
+                            'pdf':cross_pdf}
+
+
+    return paperdict, crossdict
+
+
+
+def grab_arxiv_papers(url='http://arxiv.org/list/astro-ph/new',
+                      fakery=True):
+    '''
+    This rolls up all the functions above.
+
+    '''
+
+    html = get_page_html(url, fakery=fakery)
+    soup = soupify(html)
+
+    paperlinks, paperdata, crosslinks, crossdata = get_arxiv_lists(soup)
+
+    # process the papers and crosslists
+    paperdict, crosslistdict = get_arxiv_articles(paperlinks, paperdata,
+                                                  crosslinks, crossdata)
+    now = datetime.utcnow()
+
+    return {'utc':now,
+            'npapers':len(paperdict.keys()),
+            'papers':paperdict,
+            'ncrosslists':len(crosslistdict.keys()),
+            'crosslists':crosslistdict}
