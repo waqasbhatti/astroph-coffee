1c72348f4d36a1029e2d5c4da14c51f1b48f7d61
Waqas Bhatti <wbhatti@astro.princeton.edu>
2017-11-16 15:33:57 -0500

    new tables and triggers for FTS

--- src/data/astroph-sqlite.sql
+++ src/data/astroph-sqlite.sql
@@ -43,6 +43,57 @@ create table sessions (
 );
 
 
+-- create the FTS index
+create virtual table arxiv_fts using fts4(
+       content="arxiv",
+       utcdate,
+       day_serial,
+       title,
+       article_type,
+       arxiv_id,
+       authors,
+       abstract,
+       link,
+       pdf,
+       nvotes,
+       notindexed=day_serial,
+       notindexed=link,
+       notindexed=pdf,
+       notindexed=nvotes
+);
+
+-- create the required triggers to update the FTS index whenever stuff is
+-- inserted, updated, or deleted from the arxiv table.
+create trigger fts_before_update before update on arxiv begin
+       delete from arxiv_fts where docid=old.rowid;
+end;
+
+create trigger fts_before_delete before delete on arxiv begin
+       delete from arxiv_fts where docid=old.rowid;
+end;
+
+create trigger fts_after_update after update on arxiv begin
+       insert into arxiv_fts(docid, utcdate, day_serial, title, article_type,
+                             arxiv_id, authors, abstract, link, pdf, nvotes)
+              values (new.rowid, new.utcdate, new.day_serial,
+                      new.title, new.article_type, new.arxiv_id,
+                      new.authors,
+                      new.abstract, new.link, new.pdf,
+                      new.nvotes);
+end;
+
+create trigger fts_after_insert after insert on arxiv begin
+       insert into arxiv_fts(docid, utcdate, day_serial, title, article_type,
+                             arxiv_id, authors, abstract, link, pdf, nvotes)
+              values (new.rowid, new.utcdate, new.day_serial,
+                      new.title, new.article_type, new.arxiv_id,
+                      new.authors,
+                      new.abstract, new.link, new.pdf,
+                      new.nvotes);
+end;
+
+
+
 -- SQLite specific settings
 pragma journal_mode = wal;
 pragma journal_size_limit = 52428800;
