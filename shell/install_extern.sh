d7f7daf7d77fd25b08aec42ef9293be1933c1a42
Waqas Bhatti <wbhatti@astro.princeton.edu>
2017-11-17 16:32:49 -0500

    added external pysqlite module with latest sqlite3.c included

--- shell/install_extern.sh
+++ shell/install_extern.sh
@@ -14,4 +14,25 @@ pip install itsdangerous -U
 pip install geoip2 -U
 pip install py2-ipaddress -U
 
+# install pysqlite
+export CFLAGS="-DSQLITE_ENABLE_COLUMN_METADATA \
+-DSQLITE_ENABLE_DBSTAT_VTAB \
+-DSQLITE_ENABLE_FTS3 \
+-DSQLITE_ENABLE_FTS3_PARENTHESIS \
+-DSQLITE_ENABLE_FTS4 \
+-DSQLITE_ENABLE_FTS5 \
+-DSQLITE_ENABLE_JSON1 \
+-DSQLITE_ENABLE_STAT4 \
+-DSQLITE_ENABLE_UPDATE_DELETE_LIMIT \
+-DSQLITE_SECURE_DELETE \
+-DSQLITE_SOUNDEX \
+-DSQLITE_TEMP_STORE=3 \
+-O2 \
+-fPIC"
+
+cd pysqlite
+LIBS="-lm" python setup.py build_static
+python setup.py install
+cd -
+
 deactivate
