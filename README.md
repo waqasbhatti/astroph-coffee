a85f4b6d46c10a1dbec75c7d36b9e31f67525df4
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2019-07-25 09:26:14 -0400

    initial astro-coffee2 commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Astro-coffee 2 is a rewrite of astro-coffee using:
+
+- Tornado
+- authnzerver
+- Vue
+
+Mostly to teach myself Vue and force me to finish up authnzerver.
---
+++ setup.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+'''setup.py - Waqas Bhatti (wbhatti@astro.princeton.edu) - Nov 2016
+
+This sets up the package.
+
+Stolen from http://python-packaging.readthedocs.io/en/latest/everything.html and
+modified by me.
+
+'''
+import versioneer
+__version__ = versioneer.get_version()
+
+from setuptools import setup, find_packages
+
+# set up the cmdclass
+cmdclass = versioneer.get_cmdclass()
+
+
+# get the readme
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+# let's be lazy and put requirements in one place
+# what could possibly go wrong?
+with open('requirements.txt') as infd:
+    INSTALL_REQUIRES = [x.strip('\n') for x in infd.readlines()]
+
+EXTRAS_REQUIRE = {}
+
+###############
+## RUN SETUP ##
+###############
+
+setup(
+    name='astrocoffee',
+    version=__version__,
+    cmdclass=cmdclass,
+    description=('A small Tornado webapp to review and '
+                 'vote on daily astro-ph arXiv paper listings.'),
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords='astronomy,arxiv,astro-ph,tornado',
+    url='https://github.com/waqasbhatti/astroph-coffee',
+    author='Waqas Bhatti',
+    author_email='waqas.afzal.bhatti@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
+    # entry_points={
+    #     'console_scripts':[
+    #         'astrocoffee-server=astrocoffee.main:main',
+    #     ],
+    # },
+    include_package_data=True,
+    zip_safe=False,
+    python_requires='>=3.6',
+)
