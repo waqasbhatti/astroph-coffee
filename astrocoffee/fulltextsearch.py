3bc01923a2de1d6697dfd7589bae87996762b975
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2019-08-01 16:45:51 -0400

    WIP FTS

--- astrocoffee/fulltextsearch.py
+++ astrocoffee/fulltextsearch.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-'''
+'''This contains FTS5 search functions for coffeeserver.
 
 '''
 
@@ -21,3 +21,30 @@ LOGEXCEPTION = LOGGER.exception
 #############
 ## IMPORTS ##
 #############
+
+import sqlite3
+
+
+###################
+## FTS FUNCTIONS ##
+###################
+
+def full_text_query(
+        connection,
+        query_string,
+        columns,
+        column_weights=None,
+):
+    '''This runs an FTS query.
+
+    This requires an active SQLite DBAPI connection. Get this from the
+    SQLAlchemy Connection object.
+
+    '''
+
+    # this is the query to run
+    q = ("select {columnstr} "
+         "from arxiv_fts "
+         "join arxiv_listings on (arxiv_fts.rowid = arxiv.rowid) where "
+         "arxiv_fts MATCH ? {query_string} "
+         "order by bm25(arxiv_fits{relevance_weights})")
